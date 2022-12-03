7026096d24618cc977ab1af401974420cfd25ea5
Gayan Nanayakkara <34508294+gnanayakkara@users.noreply.github.com>
2022-12-03 15:29:04 +0400

    Create Module projects and arranged

---
+++ microservices-project/inventory-service/src/main/java/com/gayan/inventoryservice/InventoryServiceApplication.java
@@ -0,0 +1,35 @@
+package com.gayan.inventoryservice;
+
+import com.gayan.inventoryservice.model.Inventory;
+import com.gayan.inventoryservice.repository.InventoryRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class InventoryServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InventoryServiceApplication.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner loadData(InventoryRepository inventoryRepository){
+		return args -> {
+			Inventory inventory = Inventory.builder()
+					.skuCode("iphone_13")
+					.quantity(100)
+					.build();
+
+			Inventory inventory1 = Inventory.builder()
+					.skuCode("iphone_13_red")
+					.quantity(0)
+					.build();
+
+			inventoryRepository.save(inventory);
+			inventoryRepository.save(inventory1);
+
+		};
+	}
+}
