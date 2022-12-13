c99a757a87c28fd648ae754f62229ea48783ea87
Gayan Nanayakkara <34508294+gnanayakkara@users.noreply.github.com>
2022-12-13 22:43:45 +0400

    Event Driven Architecture using Kafka
    Add Kafka message service to Notification

--- microservices-project/order-service/src/main/java/com/gayan/orderservice/service/OrderService.java
+++ microservices-project/order-service/src/main/java/com/gayan/orderservice/service/OrderService.java
@@ -3,12 +3,14 @@ package com.gayan.orderservice.service;
 import com.gayan.orderservice.dto.InventoryResponse;
 import com.gayan.orderservice.dto.OrderLineItemsDto;
 import com.gayan.orderservice.dto.OrderRequest;
+import com.gayan.orderservice.event.OrderPlacedEvent;
 import com.gayan.orderservice.model.Order;
 import com.gayan.orderservice.model.OrderLineItems;
 import com.gayan.orderservice.repository.OrderRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.cloud.sleuth.Span;
 import org.springframework.cloud.sleuth.Tracer;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.reactive.function.client.WebClient;
@@ -24,6 +26,7 @@ public class OrderService {
     private final OrderRepository orderRepository;
     private final WebClient.Builder webClientBuilder;
     private final Tracer tracer;
+    private final KafkaTemplate<String,OrderPlacedEvent> kafkaTemplate;
     public String placeOrder(OrderRequest orderRequest){
 
         Order order = new Order();
@@ -57,6 +60,7 @@ public class OrderService {
 
             if (isAllProductsInStock) {
                 orderRepository.save(order);
+                kafkaTemplate.send("notificationTopic",new OrderPlacedEvent(order.getOrderNumber()));
                 return "Order Placed Successfully";
             } else {
                 throw new IllegalArgumentException("Product is not in stock,please try again later");
